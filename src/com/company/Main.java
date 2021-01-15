2655cbb2ed25fe6ac1916b669b39b7c2c37a8748
artpol2001 <artpol2001@gmail.com>
2021-01-15 16:09:48 +0300

    задача 1 и 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Java!");
+        double a = 3;
+        double b = 4;
+        double c;
+        c = Math.sqrt(a* a + b* b);
+        System.out.println("c = " + c);
+    }
+}
